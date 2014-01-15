2a879765d62a8ae30de2ce3ef2d4a39d8ed5298f
Jonathan Claudius <jclaudius@trustwave.com>
2014-01-14 20:47:42 -0500

    Fix requires

--- lib/bnat.rb
+++ lib/bnat.rb
@@ -1,11 +1,8 @@
-
-
-
-#require 'bnat/version'
-#require 'bnat/common'
-#require 'bnat/tcp_packet'
-
 require 'packetfu'
+
+require 'bnat/version'
+require 'bnat/common'
+require 'bnat/tcp_packet'
 
 # PacketFu::Utils Monkey Patch
 require 'ext/utils.rb'
