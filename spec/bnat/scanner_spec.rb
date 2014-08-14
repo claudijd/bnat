4872a3c8b4ce5ad8014e8c46e0163c36de95248d
Jonathan Claudius <jclaudius@trustwave.com>
2014-08-13 20:48:57 -0400

    Re-enable scanner specs

--- spec/bnat/scanner_spec.rb
+++ spec/bnat/scanner_spec.rb
@@ -1,35 +1,30 @@
-#require 'spec_helper'
-#require 'bnat'
-#require 'packetfu'
-#
-#module Bnat
-#  describe Scanner do
-#
-#    before :each do
-#      @opts = {
-#        :iface => "eth0",
-#        :ports => [80,443],
-#        :targets => [
-#          "192.168.1.1",
-#          "192.168.1.2"
-#        ]
-#      }
-#      @scanner = Bnat::Scanner.new(@opts)
-#    end
-#
-#    context "when initializing a scanner object" do
-#      subject{@scanner}
-#
-#      its(:class) {should == Bnat::Scanner}
-#      its(:iface) {should == "eth0"}
-#      its(:ports) {should == [80,443]}
-#      its(:targets) {should == ["192.168.1.1", "192.168.1.2"]}
-#      
-#      it "should have the right pf class" do
-#        @scanner.pf.class.should == Bnat::PacketFactory
-#      end
-#      
-#    end
-#    
-#  end
-#end
+require 'spec_helper'
+require 'bnat'
+require 'packetfu'
+
+module Bnat
+ describe Scanner do
+
+   before :each do
+     @opts = {
+       :iface => "eth0",
+       :ports => [80,443],
+       :targets => [
+         "192.168.1.1",
+         "192.168.1.2"
+       ]
+     }
+     @scanner = Bnat::Scanner.new(@opts)
+   end
+
+   context "when initializing a scanner object" do
+     subject{@scanner}
+
+     its(:class) {should == Bnat::Scanner}
+     its(:iface) {should == "eth0"}
+     its(:ports) {should == [80,443]}
+     its(:targets) {should == ["192.168.1.1", "192.168.1.2"]}
+   end
+   
+ end
+end
